100afb7b7ab63f67f254210f85d14f174fada1d5
Atar09 <aliyatarahmad09@gmail.com>
2025-10-29 16:47:39 +0700

    praktikum pekan 5_2511533031

---
+++ 2025_prakalpro_C_2511533031/src/pekan5_2511533031/PerulanganFor1_2511533031.java
@@ -0,0 +1,11 @@
+package pekan5_2511533031;
+
+public class PerulanganFor1_2511533031 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
