5c7b3f1ed3715b826273d9fc6778a875869394fa
Atar09 <aliyatarahmad09@gmail.com>
2025-11-11 12:01:14 +0700

    PrakAlproPekan7_2511533031

---
+++ 2025_prakalpro_C_2511533031/src/pekan7_2511533031/PanggilMahasiswa_2511533031.java
@@ -0,0 +1,12 @@
+package pekan7_2511533031;
+
+public class PanggilMahasiswa_2511533031 {
+	public static void main(String[] args) {
+		Mahasiswa_2511533031 a= new Mahasiswa_2511533031();
+		a.setNim(251153);
+		a.setNama("Atar");
+		System.out.println(a.getNim());
+		System.out.println(a.getNama());
+		a.Cetak();
+	}
+}
